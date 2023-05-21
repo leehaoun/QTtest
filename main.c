5dc53b16144f00259e87124ff1cf7ec3cc86f41a
holee <holee@ati2000.co.kr>
2023-05-21 22:48:59 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct car
+{
+    char* name;
+    int speed;
+    int price;
+};
+
+
+int main(int argc, char *argv[])
+{
+    struct car avante;
+    avante.name = argv[2];
+    printf("Hello haoun!  %s \n", avante.name);
+    return 0;
+}
